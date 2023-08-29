cf1b11220e6dcee4dde6499ef844f4a0b0feb709
nusrat1121 <142914084+nusrat1121@users.noreply.github.com>
2023-08-29 20:27:24 +0600

    Person.java

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person {
+    public static void main(String[]args)
+    {
+        test person = new test();
+        person.name = "Jeny";
+        person.age = 21;
+        System.out.println(" name = "+person.name);
+        System.out.println(" age = "+person.age);
+    }
+}
+
+
+ class test {
+    String name;
+    int age;
+}
+
+        
