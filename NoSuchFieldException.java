2d44a3d776464c03696b85c549a204000bc23b8a
nusrat1121 <142914084+nusrat1121@users.noreply.github.com>
2023-10-19 14:58:26 +0600

    Create NoSuchFieldException.java

---
+++ NoSuchFieldException.java
@@ -0,0 +1,19 @@
+// Java program to handle NoSuchFieldException
+
+import java.lang.reflect.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      String obj = new String();
+      Class cls = obj.getClass();
+
+      Field fld = cls.getField("name");
+      System.out.println("Field found: " + fld.toString());
+    } catch (NoSuchFieldException e) {
+      System.out.println("Exception: " + e);
+    }
+
+    System.out.println("Program finished");
+  }
+}
