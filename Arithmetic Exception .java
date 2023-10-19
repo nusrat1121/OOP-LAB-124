41c397f13578bfd7853234f3c842bab5196b1c61
nusrat1121 <142914084+nusrat1121@users.noreply.github.com>
2023-10-19 14:32:43 +0600

    Create Arithmetic Exception .java

---
+++ Arithmetic Exception .java
@@ -0,0 +1,28 @@
+// Java program to demonstrate ArithmeticException 
+
+package com.mycompany.mavenproject10;
+
+public class ArithmeticException {
+ public static void main(String args[]) 
+
+    { 
+        try { 
+
+            int a = 30, b = 0; 
+
+            int c = a/b;  // cannot divide by zero 
+
+            System.out.println ("Result = " + c); 
+
+        } 
+
+        catch(Exception e) { 
+
+            System.out.println ("Can't divide a number by 0"); 
+
+        }
+    }
+}
+
+
+
