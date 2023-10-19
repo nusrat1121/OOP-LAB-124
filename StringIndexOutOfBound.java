723606a32e9e71f607d9f9be4d334587b9abb1f3
nusrat1121 <142914084+nusrat1121@users.noreply.github.com>
2023-10-19 14:47:12 +0600

    Create StringIndexOutOfBound.java

---
+++ StringIndexOutOfBound.java
@@ -0,0 +1,26 @@
+// Java program to demonstrate StringIndexOutOfBoundsException 
+package com.mycompany.mavenproject19;
+public class StringIndexOutOfBound {
+  
+    public static void main(String args[]) 
+
+    { 
+        try { 
+
+            String a = "This is like chipping "; // length is 22 
+
+            char c = a.charAt(24); // accessing 25th element 
+
+            System.out.println(c); 
+        } 
+
+        catch(StringIndexOutOfBoundsException e) { 
+            System.out.println("StringIndexOutOfBoundsException"); 
+
+        } 
+
+    } 
+} 
+
+    
+    
